8ddcfdb3a1140b9761498bde937e8d45439097d5
Phil Chen <nethacker@gmail.com>
2025-04-20 11:25:12 -0700

    Adding improvments to general profiler

--- tools/config_tool.py
+++ tools/config_tool.py
@@ -2,9 +2,8 @@
 
 import os
 import sys
-from service_profiler import service_profiler
+from process_profiler_general import process_profiler_general
 sys.dont_write_bytecode = True
-
 
 # Menu ANSI Colors
 BLACK = '\033[30m'
@@ -68,7 +67,7 @@ def display_menu():
         print(f"\n{BRIGHT_GREEN}PROCESS WATCH OPTIONS{RESET}\n")
         print("1. List Configuration Files.")
         print("2. Delete a Configuration.")
-        print("3. Create a Service Profile Configuration.")
+        print("3. Create a Process Profile Configuration.")
         print("4. Quit")
         
         top_choice = input(f"\n{BRIGHT_CYAN}Enter your choice (1-4):{RESET}\n")
@@ -97,17 +96,16 @@ def display_menu():
             clear_screen()
             while True:
                 print(f"\n{BRIGHT_GREEN}PROCESS WATCH CONFIGURATION CREATION OPTIONS{RESET}\n")
-                print("1. Create Service Profiler")
-                print("2. Create TBD")
+                print("1. Create General Profiler")
+                print("2. Create Memory Profiler")
                 print("3. Exit")
                 watch_list_choice = input(f"\n{BRIGHT_CYAN}Enter your choice (1-3):{RESET}\n")
                 if watch_list_choice == '1':
                     clear_screen()
-                    service_profiler()
+                    process_profiler_general()
                     break
                 if watch_list_choice == '2':
                     clear_screen()
-                    print('TBD')
                     break
                 if watch_list_choice == '3':
                     break
---
+++ tools/process_profiler_general.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+
+import os
+import textwrap
+import subprocess
+
+# Menu ANSI Colors
+BLACK = '\033[30m'
+GREEN = '\033[32m'
+BRIGHT_GREEN = '\033[92m'
+BRIGHT_CYAN = '\033[96m'
+BRIGHT_YELLOW = '\033[93m'
+BACKGROUND_BRIGHT_MAGENTA = '\033[105m'
+RESET = '\033[0m'
+
+# Reset Sceen
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+# Identify Process By Name Return PID
+def find_pid_by_name(name):
+    all_pids = subprocess.Popen(['pgrep', '-f', name], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    output, errors = all_pids.communicate()
+    all_pids_array = output.strip().split("\n")
+    first_pid = all_pids_array[0]
+    return first_pid
+
+# Write configuration file to watch_list directory
+def write_to_file(filename, template):
+    try:
+        with open(os.path.join(os.pardir, "watch_list", filename), 'w', encoding="utf-8") as file:
+            file.write(template)
+        clear_screen()
+        print(f"\n{GREEN}'{filename}' configuration created.{RESET}")
+    except Exception as e:
+        print(f"{BLACK}{BACKGROUND_BRIGHT_MAGENTA}\nAn error occurred: {e}{RESET}")
+
+# Process Profiler General Menu
+def process_profiler_general():
+    print(f"\n{BRIGHT_GREEN}SERVICE PROFILER GENERAL SETTINGS:{RESET}")
+    filename = input(f"\n{BRIGHT_CYAN}Enter the name of the configuration file:{RESET}\n")
+    sanitized_filename = filename.replace(".", "-")
+    process_name = input(f"\n{BRIGHT_CYAN}Enter the process name to monitor:{RESET}\n")
+    process_id = int(find_pid_by_name(process_name))
+    interval = input(f"\n{BRIGHT_CYAN}Enter the monitoring interval in seconds:{RESET}\n")
+    # Service Profiler General Template
+    template = f"""
+    import os
+    import sys
+    import time
+    import logging
+    import threading
+    import psutil
+    import subprocess
+
+    output_file = (f"{{str(os.getcwd())}}/logs/process_watch.log")
+
+    # Identify Process By Name Return PID
+    def find_pid_by_name(name):
+        all_pids = subprocess.Popen(['pgrep', '-f', name], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        output, errors = all_pids.communicate()
+        all_pids_array = output.strip().split("\\n")
+        first_pid = all_pids_array[0]
+        return first_pid
+
+    process_id = int(find_pid_by_name("{process_name}"))
+
+    def find_memory_usage_by_pid(pid):
+        process = psutil.Process(pid)
+        memory_info = process.memory_info()
+        rss_bytes = memory_info.rss
+        rss_megabytes = int(rss_bytes / (1024 * 1024))
+        if rss_megabytes:
+            return rss_megabytes
+        else:
+            return ("Process N/A.")
+
+    def find_cpu_usage_by_pid(pid):
+        process = psutil.Process(pid)
+        cpu_percentage = process.cpu_percent(interval=1)
+        if cpu_percentage:
+            return cpu_percentage
+        else:
+            return ("0.0")
+
+    def monitor():
+
+        while True:
+            try:
+                with open(output_file, "a", encoding="utf-8") as f:
+                    f.write(f"{process_name} running at local time {{time.ctime()}} PID: {process_id}, Memory: {{find_memory_usage_by_pid({process_id})}}MB, CPU: {{find_cpu_usage_by_pid({process_id})}}%\\n")
+                time.sleep({interval})
+            except Exception as e:
+                logging.error("An error occurred in Process Watch: %s", e, exc_info=True)
+                raise sys.exit(1)
+
+    def worker():
+        monitor_thread = threading.Thread(target=monitor)
+        monitor_thread.start()
+
+    worker()
+    """
+    # Write the template into a  config
+    write_to_file(os.path.abspath(f"../watch_list/{sanitized_filename}.py"), textwrap.dedent(template))
